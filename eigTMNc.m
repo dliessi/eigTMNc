2f26646a20efeb5c8219907b0c6e0d53372ff5d2
Davide Liessi <liessi.davide@spes.uniud.it>
2018-01-23 00:26:30 +0100

    add GPL3+ license

--- eigTMNc.m
+++ eigTMNc.m
@@ -41,6 +41,21 @@ function [mu, saved] = eigTMNc(model, par, s, h, M, N, varargin)
 %            University of Udine, Italy
 %
 %   v0.3 2017-10-23
+%
+%   Copyright (C) 2017 Davide Liessi
+%
+%   This program is free software: you can redistribute it and/or modify
+%   it under the terms of the GNU General Public License as published by
+%   the Free Software Foundation, either version 3 of the License, or
+%   (at your option) any later version.
+%
+%   This program is distributed in the hope that it will be useful,
+%   but WITHOUT ANY WARRANTY; without even the implied warranty of
+%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%   GNU General Public License for more details.
+%
+%   You should have received a copy of the GNU General Public License
+%   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 %% Check sanity of inputs.
